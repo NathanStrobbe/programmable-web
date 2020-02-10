6766a972f287402e45cf6fa43cd48be30f8d0f93
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2020-02-10 16:11:02 +0100

    Improve css

--- front-flms/src/components/PluginDetails.js
+++ front-flms/src/components/PluginDetails.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Button, Row, Col, Card, Container, Badge, Form } from 'react-bootstrap';
-import { GetPlugin,AddLike, GetUser, GetComments,AddComment, convertBufferToBase64 } from '../utils/hooks.js';
-import {useDispatch, useSelector} from 'react-redux';
+import { GetPlugin, AddLike, GetUser, GetComments, AddComment, convertBufferToBase64 } from '../utils/hooks.js';
+import { useSelector } from 'react-redux';
+import './PluginDetails.css';
 
 const PluginDetails = () => {
     //const [plugin, setPlugin] = useState({name: '', version: '', category: '', image: '', description: '', tags: [], likes: []});
@@ -18,10 +19,10 @@ const PluginDetails = () => {
     const click = (plugin) => {
         if (sessionStorage.getItem('jwtToken')) {
             const myId = user._id;
-            if(!plugin.likes.includes(myId)){
-                AddLike(plugin,myId);
+            if (!plugin.likes.includes(myId)) {
+                AddLike(plugin, myId);
                 window.location.reload();
-            }else {
+            } else {
                 alert('Vous avez deja aimé !');
             }
         } else {
@@ -30,16 +31,16 @@ const PluginDetails = () => {
     };
 
     const handleSubmit = (e) => {
-      e.preventDefault();
-      const data = new FormData(e.target);
-      const comment = Object.fromEntries(data);
-      if (sessionStorage.getItem('jwtToken')) {
-          const myId = user.username;
-          AddComment(plugin,myId, comment.commentContent);
-          window.location.reload();
-      } else {
-          alert('Veuillez vous connecter !');
-      }
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const comment = Object.fromEntries(data);
+        if (sessionStorage.getItem('jwtToken')) {
+            const myId = user.username;
+            AddComment(plugin, myId, comment.commentContent);
+            window.location.reload();
+        } else {
+            alert('Veuillez vous connecter !');
+        }
     };
 
     const plugin = GetPlugin(pluginId);
@@ -53,11 +54,11 @@ const PluginDetails = () => {
             <Container className="pluginDetails">
                 <Row className="pluginDetailsHeader">
                     <Col><h1>{plugin.name}</h1></Col>
-                    <Col><img src={convertBufferToBase64(plugin.image)} /></Col>
+                    <Col><img className="Image" src={convertBufferToBase64(plugin.image)} /></Col>
                     <Col><h4>Likes : {plugin.likes.length}</h4><Button variant="primary" onClick={e => click(plugin)}>Add</Button></Col>
                 </Row>
                 <Row className="pluginDetailsSourceLink">
-                    <Col><a  href={plugin.linkgithub ? plugin.linkgithub : ''}>{plugin.linkgithub ? plugin.linkgithub : ''}</a></Col>
+                    <Col><a href={plugin.linkgithub ? plugin.linkgithub : ''}>{plugin.linkgithub ? plugin.linkgithub : ''}</a></Col>
                 </Row>
                 <Row className="pluginDetailsPicture">
                     <Col></Col>
@@ -69,35 +70,35 @@ const PluginDetails = () => {
                     <Col>{plugin.tags.map((tag) => <><Badge variant="info">{tag}</Badge></>)}</Col>
                     <Col></Col>
                 </Row>
-                <br/>
+                <br />
                 <Row className="pluginDetailsDescriptionTitle">
                     <Col><h4>Description:</h4></Col>
                 </Row>
                 <Row className="pluginDetailsDescription">
                     <Col>{plugin.description}</Col>
                 </Row>
-                <br/>
+                <br />
                 <Row className="pluginDetailsCommentsTitle">
                     {loggedIn ?
-                      <Col>
-                      <Form onSubmit={handleSubmit}>
-                        <Form.Group>
-                            <Form.Label htmlFor="commentContent">Ajouter un commentaire</Form.Label>
-                            <Form.Control name="commentContent" id="commentContent"/>
-                        </Form.Group>
-                        <Button type="submit" /*onClick={e => handleSubmit(plugin,comment)}*/>Ajouter</Button>
-                        </Form>
-                    </Col>
-                    :
-                    <Col></Col>
-                  }
+                        <Col>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group>
+                                    <Form.Label htmlFor="commentContent">Ajouter un commentaire</Form.Label>
+                                    <Form.Control name="commentContent" id="commentContent" />
+                                </Form.Group>
+                                <Button type="submit" /*onClick={e => handleSubmit(plugin,comment)}*/>Ajouter</Button>
+                            </Form>
+                        </Col>
+                        :
+                        <Col></Col>
+                    }
                 </Row>
-                <Row className="pluginDetailsAddCommentsTitle">
-                  {loggedIn ?
-                    <Col><h4>Comments:</h4></Col>
-                  :
-                    <Col><h4>Comments (connectez vous pour commenter):</h4></Col>
-                  }
+                <Row className="mt-3 pluginDetailsAddCommentsTitle">
+                    {loggedIn ?
+                        <Col><h4>Comments:</h4></Col>
+                        :
+                        <Col><h4>Comments (connectez vous pour commenter):</h4></Col>
+                    }
                 </Row>
                 {
                     comments.length > 0 ? comments.map((comment, i) => {
@@ -109,12 +110,12 @@ const PluginDetails = () => {
                                         <Card.Body>
                                             <Card.Title>
                                                 <div>
-                                                    <span style={{float: 'left'}}><b>{comment.writer}</b></span>
-                                                    <span style={{float: 'right'}}>
+                                                    <span style={{ float: 'left' }}><b>{comment.writer}</b></span>
+                                                    <span style={{ float: 'right' }}>
                                                         {commentDate.toLocaleDateString()}
                                                     </span>
                                                 </div>
-                                                <br/>
+                                                <br />
                                             </Card.Title>
                                             <Card.Text>
                                                 {comment.content}
@@ -122,11 +123,11 @@ const PluginDetails = () => {
                                         </Card.Body>
                                     </Card>
                                 </Row>
-                                <br/>
+                                <br />
                             </>
                         );
                     }
-                    ) : <br/>
+                    ) : <br />
                 }
             </Container>
         );
