4b5d1174a4b95f1d312eef2bf3e605e23fb9c993
CamilleLeRoux <camille.le-roux1@etu.unice.fr>
2020-02-13 11:15:37 +0100

    #19 redirection en fonction du login

---
+++ front-flms/src/components/Shop.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+
+const Shop = () => {
+
+  return (
+        <div className="shop content">Yolo</div>
+    );
+};
+
+export default Shop;
