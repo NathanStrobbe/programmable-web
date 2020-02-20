26879f850937598ce8e647aeceaab5f680b00377
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2020-02-20 08:43:38 +0100

    #25 done retour details plugin

--- front-flms/src/components/PluginDetails.js
+++ front-flms/src/components/PluginDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Button, Row, Card, Container, Badge, Form } from 'react-bootstrap';
+import {NavLink, useHistory, useParams} from 'react-router-dom';
+import {Button, Row, Card, Container, Badge, Form, Breadcrumb} from 'react-bootstrap';
 import { convertBufferToBase64 } from '../utils/utils';
 import { useSelector } from 'react-redux';
 import SweetAlert from 'sweetalert2-react';
@@ -8,6 +8,8 @@ import './PluginDetails.css';
 import heartFill from '../assets/heart.png';
 import heartBlank from '../assets/heart_blank.png';
 import { get, post } from '../utils/api.js';
+import Navbar from "react-bootstrap/esm/Navbar";
+import PluginsList from "./PluginsList";
 
 const defaultPlugin = {
     name: '',
@@ -35,6 +37,8 @@ const defaultUser = {
 };
 
 const PluginDetails = () => {
+
+    const history = useHistory();
     const { pluginId } = useParams();
 
     const [user, setUser] = useState(defaultUser);
@@ -126,49 +130,57 @@ const PluginDetails = () => {
 
     if (plugin === defaultPlugin) return null;
 
-    return (
-        <Container className="pluginDetails">
-            <div className="detailsHeader">
-                <img className="Image" src={convertBufferToBase64(plugin.image)} alt="Plugin" />
-                <div className="detailsText">
-                    <div className="detailsGroup">
-                        <h3>{plugin.name} {plugin.version}</h3>
-                        <img onClick={() => click(plugin)} src={heart()} alt="Add a like" width="20" height="20px" style={{ cursor: 'pointer' }} />
-                        {plugin.likes.length}
-                    </div>
-                    <p>Auteur : {plugin.creator.username}</p>
+    const handleBackButton = () =>{
+        history.push('/pluginsList');
+    };
 
-                    <p>Catégorie : {category.name}</p>
+    return (
+        <div>
+            <Breadcrumb>
+                <Breadcrumb.Item onClick={handleBackButton}>Liste des plugins</Breadcrumb.Item>
+                <Breadcrumb.Item active>Détail</Breadcrumb.Item>
+            </Breadcrumb>
+            <Container className="pluginDetails">
+                <div className="detailsHeader">
+                    <img className="Image" src={convertBufferToBase64(plugin.image)} alt="Plugin" />
+                    <div className="detailsText">
+                        <div className="detailsGroup">
+                            <h3>{plugin.name} {plugin.version}</h3>
+                            <img onClick={() => click(plugin)} src={heart()} alt="Add a like" width="20" height="20px" style={{ cursor: 'pointer' }} />
+                            {plugin.likes.length}
+                        </div>
+                        <p>Auteur : {plugin.creator.username}</p>
+
+                        <p>Catégorie : {category.name}</p>
+                        <div>
+                            {
+                                plugin.tags.map((tag, i) => <Badge key={i} variant="info">{tag}</Badge>)
+                            }
+                        </div>
+                        <a href={plugin.video ? plugin.video : ''} target="_blank" rel="noopener noreferrer"> {plugin.video ? 'Vidéo' : ''} </a>
+                        <a href={plugin.linkgithub ? plugin.linkgithub : ''}> {plugin.linkgithub ? 'GitHub' : ''} </a>
+                    </div>
+                </div>
+
+                <div className="detailsDescription">
+                    <h5>Description</h5>
+                    <p>{plugin.description}</p>
+                    <Button variant="outline-secondary" onClick={() => window.open(`http://localhost:8000/plugins/${encodeURI(plugin.name)}?dt=${new Date().getTime()}`, '_blank')} >Essayer le plugin</Button>
+                    <Button variant="outline-secondary" onClick={() => window.open(`http://localhost:8000/testers/testPluginWithMocha.html?urlPlugin=http://localhost:8000/plugins/${encodeURI(plugin.name)}`, '_blank')} >Valider le plugin</Button>
+                </div>
+
+                <div className="detailsComments">
                     <div>
-                        {
-                            plugin.tags.map((tag, i) => <Badge key={i} variant="info">{tag}</Badge>)
+                        {loggedIn ?
+                            <h5>Commentaires :</h5>
+                            :
+                            <h5>Commentaires (connectez vous pour commenter):</h5>
                         }
                     </div>
-                    <a href={plugin.video ? plugin.video : ''} target="_blank" rel="noopener noreferrer"> {plugin.video ? 'Vidéo' : ''} </a>
-                    <a href={plugin.linkgithub ? plugin.linkgithub : ''}> {plugin.linkgithub ? 'GitHub' : ''} </a>
-                </div>
-            </div>
-
-            <div className="detailsDescription">
-                <h5>Description</h5>
-                <p>{plugin.description}</p>
-                <Button variant="outline-secondary" onClick={() => window.open(`http://localhost:8000/plugins/${encodeURI(plugin.name)}?dt=${new Date().getTime()}`, '_blank')} >Essayer le plugin</Button>
-                <Button variant="outline-secondary" onClick={() => window.open(`http://localhost:8000/testers/testPluginWithMocha.html?urlPlugin=http://localhost:8000/plugins/${encodeURI(plugin.name)}`, '_blank')} >Valider le plugin</Button>
-            </div>
-
-            <div className="detailsComments">
-                <div>
-                    {loggedIn ?
-                        <h5>Commentaires :</h5>
-                        :
-                        <h5>Commentaires (connectez vous pour commenter):</h5>
-                    }
-                </div>
-                {
-                    comments.length > 0 ? comments.map((comment, i) => {
-                        const commentDate = new Date(comment.date);
-                        return (
-                            <>
+                    {
+                        comments.length > 0 ? comments.map((comment, i) => {
+                            const commentDate = new Date(comment.date);
+                            return (
                                 <Row key={i} className="pluginDetailsComment">
                                     <Card className="w-100">
                                         <Card.Body>
@@ -187,31 +199,31 @@ const PluginDetails = () => {
                                         </Card.Body>
                                     </Card>
                                 </Row>
-                            </>
-                        );
+                            );
+                        }
+                        ) : <br />
                     }
-                    ) : <br />
-                }
-                {loggedIn ?
-                    <Form onSubmit={handleSubmit} className="detailsAddComment">
-                        <Form.Group>
-                            <Form.Label htmlFor="commentContent">Ajouter un commentaire</Form.Label>
-                            <Form.Control as="textarea" rows="3" name="commentContent" id="commentContent" required />
-                        </Form.Group>
-                        <Button type="submit">Commenter</Button>
-                    </Form>
-                    :
-                    <p></p>
-                }
-            </div>
-            <SweetAlert
-                show={alertMessage}
-                title="Erreur"
-                text={alertMessage}
-                type="error"
-                onConfirm={() => setAlertMessage('')}
-            />
-        </Container>
+                    {loggedIn ?
+                        <Form onSubmit={handleSubmit} className="detailsAddComment">
+                            <Form.Group>
+                                <Form.Label htmlFor="commentContent">Ajouter un commentaire</Form.Label>
+                                <Form.Control as="textarea" rows="3" name="commentContent" id="commentContent" required />
+                            </Form.Group>
+                            <Button type="submit">Commenter</Button>
+                        </Form>
+                        :
+                        <p></p>
+                    }
+                </div>
+                <SweetAlert
+                    show={alertMessage}
+                    title="Erreur"
+                    text={alertMessage}
+                    type="error"
+                    onConfirm={() => setAlertMessage('')}
+                />
+            </Container>
+        </div>
     );
 };
 
